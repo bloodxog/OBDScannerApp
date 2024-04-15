7eba5cd53522068a7471ff4aa85c4c8575561492
bloodxog <sonofdarvi4@gmail.com>
2024-04-15 16:17:13 +0300

    Добавлена динамика разметки относительно состояния Bluetooth

--- BtDef/src/main/java/com/example/btdef/DeviceListFragment.kt
+++ BtDef/src/main/java/com/example/btdef/DeviceListFragment.kt
@@ -1,19 +1,26 @@
 package com.example.btdef
 
+import android.app.Activity
 import android.bluetooth.BluetoothAdapter
 import android.bluetooth.BluetoothManager
 import android.content.Context
+import android.content.Intent
 import android.graphics.Color
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import com.example.btdef.databinding.FragmentListBinding
+import com.google.android.material.snackbar.Snackbar
 
 class DeviceListFragment : Fragment() {
     private var bAdapter: BluetoothAdapter? = null
     private lateinit var binding: FragmentListBinding
+    private lateinit var btLauncher: ActivityResultLauncher<Intent>
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -24,6 +31,10 @@ class DeviceListFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        registerBtLauncher()
+        binding.imBluetoothOn.setOnClickListener {
+            btLauncher.launch(Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE))
+        }
         initBtAdapter()
         bluetoothState()
     }
@@ -35,6 +46,20 @@ class DeviceListFragment : Fragment() {
     private fun bluetoothState(){
         if (bAdapter?.isEnabled == true) {
             changeButtonColor(binding.imBluetoothOn, Color.GREEN)
+        }
+    }
+
+    private fun registerBtLauncher(){
+        btLauncher = registerForActivityResult(
+            ActivityResultContracts.StartActivityForResult()
+        ){
+            if (it.resultCode == Activity.RESULT_OK){
+                changeButtonColor(binding.imBluetoothOn, Color.GREEN)
+                Snackbar.make(binding.root, "Bluetooth включен!", Snackbar.LENGTH_LONG).show()
+            } else {
+                Snackbar.make(binding.root, "Необходимо включить Bluetooth!", Snackbar.LENGTH_LONG).show()
+            }
+
         }
     }
 }
