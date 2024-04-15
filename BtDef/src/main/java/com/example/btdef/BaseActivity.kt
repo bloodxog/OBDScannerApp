0dab8d69202020b142f5c45dd9dc5bee95592ca7
bloodxog <sonofdarvi4@gmail.com>
2024-04-15 15:38:01 +0300

    Добавлена логика адаптера для списков устройств

--- BtDef/src/main/java/com/example/btdef/BaseActivity.kt
+++ BtDef/src/main/java/com/example/btdef/BaseActivity.kt
@@ -5,16 +5,34 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class BaseActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_base)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        setContentView(R.layout.fragment_list)
+        initRcView()
+    }
+
+    private fun initRcView(){
+        val rcView = findViewById<RecyclerView>(R.id.rcViewPaired)
+        rcView.layoutManager = LinearLayoutManager(this)
+        val adapter = ItemAdapter()
+        rcView.adapter = adapter
+        adapter.submitList(createDeviceList())
+    }
+
+    private fun createDeviceList(): List<ListItem>{
+        val list = ArrayList<ListItem>()
+        for (i in 0 until 5){
+            list.add(
+                ListItem(
+                    "Device $i",
+                    "34:56:89:56"
+                )
+            )
         }
+        return list
     }
 }
---
+++ BtDef/src/main/java/com/example/btdef/ListItem.kt
@@ -0,0 +1,6 @@
+package com.example.btdef
+
+data class ListItem(
+    val name: String,
+    val mac: String
+)
